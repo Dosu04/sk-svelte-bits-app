ce3415856fce786b9d168de65b502c4207322627
Emmanuel Oluwafunso Oladosu <87922887+Dosu04@users.noreply.github.com>
2023-04-06 01:34:55 -0800

    up

---
+++ src/routes/+page.svelte
@@ -0,0 +1,16 @@
+
+
+<main>
+    <h1>SvelteBits</h1>
+<p>A collaborative code snippet sharing web app that allows developers to share and discover code snippets created using Svelte.
+
+</p>
+
+</main>
+
+<style>
+    main {
+        display: grid;
+        place-items: center;
+    }
+</style>
